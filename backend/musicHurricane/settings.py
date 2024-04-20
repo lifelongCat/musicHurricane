3b71ef9708b5282b3b76246316f62bbc5001e38d
lifelongCat <lifelongCat@yandex.ru>
2024-04-20 12:36:41 +0300

    move django secret key to .env

--- backend/musicHurricane/settings.py
+++ backend/musicHurricane/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uh3xcu1w3%dj9&)vbw*2m$6ej1hrdeo9ei&39j3b3q*8i--$=f'
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
