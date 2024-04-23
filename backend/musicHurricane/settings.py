68ddfa03c6bb6540618273cd790b368d6a17433f
lifelongCat <lifelongCat@yandex.ru>
2024-04-23 09:59:35 +0300

    create musicians app

--- backend/musicHurricane/settings.py
+++ backend/musicHurricane/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'apps.musicians.apps.MusiciansConfig',
 ]
 
 MIDDLEWARE = [
