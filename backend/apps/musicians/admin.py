d52d2f87cbaeb74621d55427d8d14971fd013f7f
lifelongCat <lifelongcat@yandex.ru>
2024-04-24 15:03:21 +0300

    create basic admin panel (musicians, compositions and awards)

--- backend/apps/musicians/admin.py
+++ backend/apps/musicians/admin.py
@@ -1,3 +1,105 @@
 from django.contrib import admin
+from .models import Award, Composition, Musician, MusicianComposition
 
-# Register your models here.
+
+class AwardInline(admin.TabularInline):
+    model = Award
+    verbose_name = 'award'
+    verbose_name_plural = 'awards'
+    extra = 1
+
+
+class MusicianCompositionInline(admin.TabularInline):
+    model = MusicianComposition
+    verbose_name = 'composition'
+    verbose_name_plural = 'compositions'
+    extra = 1
+
+
+@admin.register(Award)
+class AwardAdmin(admin.ModelAdmin):
+    # add "save as new" button
+    save_as = True
+
+    # add and change pages
+    fields = ('id', 'title', 'musician', 'year')
+    readonly_fields = ('id',)
+
+    # view page
+    list_display = ('title', 'musician', 'year')
+    list_display_links = ('title',)
+
+    ordering = ('title', 'musician')
+    sortable_by = ('title', 'year', 'musician')
+
+    list_filter = ('title', 'musician__first_name', 'musician__last_name', 'year')
+    search_fields = ('title', 'musician__first_name', 'musician__last_name')
+    search_help_text = 'search by title or musician'
+
+
+@admin.register(Composition)
+class CompositionAdmin(admin.ModelAdmin):
+    # add "save as new" button
+    save_as = True
+
+    # add and change pages
+    fields = ('id', 'title', 'genre', 'duration')
+    readonly_fields = ('id',)
+
+    # view page
+    list_display = ('title', 'genre', 'duration')
+    list_display_links = ('title',)
+
+    ordering = ('title', 'genre', '-duration')
+    sortable_by = ('title', 'genre', 'duration')
+
+    list_filter = ('title', 'genre', 'duration')
+    search_fields = ('title',)
+    search_help_text = 'search by title'
+
+    # many to many inline (add and change page)
+    inlines = (MusicianCompositionInline,)
+
+
+@admin.register(Musician)
+class MusicianAdmin(admin.ModelAdmin):
+    # add "save as new" button
+    save_as = True
+
+    # add and change pages
+    fields = ('id', 'first_name', 'last_name', 'birth_date')
+    readonly_fields = ('id',)
+
+    # view page
+    list_display = ('first_name', 'last_name', 'birth_date')
+    list_display_links = ('first_name', 'last_name')
+
+    ordering = ('first_name', 'last_name', 'birth_date')
+    sortable_by = ('first_name', 'last_name', 'birth_date')
+
+    list_filter = ('first_name', 'last_name', 'birth_date')
+    search_fields = ('first_name', 'last_name')
+    search_help_text = 'search by first or last name'
+
+    # one to many and many to many inlines (add and change page)
+    inlines = (AwardInline, MusicianCompositionInline)
+
+
+@admin.register(MusicianComposition)
+class MusicianCompositionAdmin(admin.ModelAdmin):
+    # add "save as new" button
+    save_as = True
+
+    # add and change pages
+    fields = ('musician', 'composition')
+
+    # view page
+    list_display = ('musician', 'composition')
+    list_display_links = ('musician', 'composition')
+
+    ordering = ('musician', 'composition')
+    sortable_by = ('musician', 'composition')
+
+    list_filter = ('musician__first_name', 'musician__last_name', 'composition__title', 'composition__genre')
+    search_fields = ('musician__first_name', 'musician__last_name', 'composition__title')
+    search_help_text = 'search by musician first name, musician last name or composition title'
--- backend/apps/musicians/apps.py
+++ backend/apps/musicians/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MusiciansConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.musicians'
+    verbose_name = 'musicians'
