2f7f6b375881c259b552be6b8bf377e342f7fff3
lifelongCat <lifelongcat@yandex.ru>
2024-05-27 17:34:27 +0300

    fix linter errors for last commit

--- backend/apps/musicians/urls.py
+++ backend/apps/musicians/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from .views import CompositionViewSet
 
 router = DefaultRouter()
-router.register(r'compositions', CompositionViewSet)
+router.register('compositions', CompositionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/apps/musicians/views.py
+++ backend/apps/musicians/views.py
@@ -1,9 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from .serializers import CompositionSerializer
+
 from .models import Composition
+from .serializers import CompositionSerializer
 
 
 class CompositionViewSet(ModelViewSet):
     queryset = Composition.objects.all()
     serializer_class = CompositionSerializer
-
