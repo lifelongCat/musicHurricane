e696d7ffaa912154e6b6ef8e458c2fd9b3fe1788
lifelongCat <lifelongcat@yandex.ru>
2024-05-28 21:05:16 +0300

    create endpoint for download composition .mp3 file

--- backend/apps/musicians/urls.py
+++ backend/apps/musicians/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CompositionViewSet
+from .views import CompositionViewSet, DownloadComposition
 
 router = DefaultRouter()
 router.register('compositions', CompositionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('download/<composition_id>/', DownloadComposition.as_view()),
 ]
--- backend/apps/musicians/views.py
+++ backend/apps/musicians/views.py
@@ -1,3 +1,9 @@
+from uuid import UUID
+
+from django.http import FileResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Composition
@@ -7,3 +13,11 @@ from .serializers import CompositionSerializer
 class CompositionViewSet(ModelViewSet):
     queryset = Composition.objects.all()
     serializer_class = CompositionSerializer
+
+
+class DownloadComposition(APIView):
+    def get(self, request, composition_id: UUID):
+        try:
+            return FileResponse(open(f'./songs/{composition_id}.mp3', 'rb'))
+        except FileNotFoundError:
+            return Response({'detail': 'Composition not found'}, status=status.HTTP_404_NOT_FOUND)
