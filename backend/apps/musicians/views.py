1df47fca4e7eda46544451618bcd58648d31c15f
lifelongCat <lifelongcat@yandex.ru>
2024-05-31 10:39:16 +0300

    delete unused download composition file endpoint

--- backend/apps/musicians/views.py
+++ backend/apps/musicians/views.py
@@ -13,11 +13,3 @@ from .serializers import CompositionSerializer
 class CompositionViewSet(ModelViewSet):
     queryset = Composition.objects.all()
     serializer_class = CompositionSerializer
-
-
-class DownloadComposition(APIView):
-    def get(self, request, composition_id: UUID):
-        try:
-            return FileResponse(open(f'./songs/{composition_id}.mp3', 'rb'))
-        except FileNotFoundError:
-            return Response({'detail': 'Composition not found'}, status=status.HTTP_404_NOT_FOUND)
