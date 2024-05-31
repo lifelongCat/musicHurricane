4b88f3424273054a6263be613d26ddcc2093e969
lifelongCat <lifelongcat@yandex.ru>
2024-05-31 10:41:06 +0300

    delete unused imports in musician views

--- backend/apps/musicians/views.py
+++ backend/apps/musicians/views.py
@@ -1,9 +1,3 @@
-from uuid import UUID
-
-from django.http import FileResponse
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Composition
