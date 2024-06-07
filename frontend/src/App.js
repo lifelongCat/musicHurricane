e17779372f23daa7fa8a8fb63edbfc185f69ec7c
lifelongCat <lifelongcat@yandex.ru>
2024-06-07 16:57:24 +0300

    add compositions loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,33 @@ import CompositionService from "./API/CompositionService";
 
 function App() {
     const [songs, setSongs] = useState([])
+    const [isCompositionsLoading, setCompositionsLoading] = useState(true);
     const [currentSongIndex, setCurrentSongIndex] = useState(0);
 
     async function fetchSongs() {
         const response = await CompositionService.getAll();
         setSongs(response.data);
+        setCompositionsLoading(false);
     }
 
     useEffect(() => {
         fetchSongs();
     }, []);
 
-    return !songs.length ?
-        <h1>Загрузка плеера</h1> :
-        (
-            <div className="App">
-                <Player
-                    songs={songs}
-                    currentSongIndex={currentSongIndex}
-                    setCurrentSongIndex={setCurrentSongIndex}
-                />
-            </div>
-        );
+    return (
+        <div className="App">
+            {isCompositionsLoading
+                ? <h1>Загрузка плеера</h1>
+                : !songs.length
+                    ? <h1>Нет ни одного произведения</h1>
+                    : <Player
+                        songs={songs}
+                        currentSongIndex={currentSongIndex}
+                        setCurrentSongIndex={setCurrentSongIndex}
+                    />
+            }
+        </div>
+    );
 }
 
 export default App;
